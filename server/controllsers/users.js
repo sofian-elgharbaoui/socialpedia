df3228f49ad17504f23140e4360dd02f6601d5fc
sofian-elgharbaoui <sofiangharbaoui54@gmail.com>
2023-03-02 23:12:27 +0100

    finsh from all users controllers

--- server/controllsers/users.js
+++ server/controllsers/users.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestErr } = require("../errors/errors_index");
 const User = require("../models/User");
+const formattedFriendsFn = require("../helpers/formattedFriends");
 
 const getUser = async (req, res) => {
   const { id } = req.params;
@@ -10,7 +11,8 @@ const getUser = async (req, res) => {
     throw new BadRequestErr(`there is no user with this id: ${id}`);
   }
 
-  res.status(StatusCodes.OK).json(userInfo);
+  userInfo.friends = await formattedFriendsFn(userInfo.friends);
+  res.status(StatusCodes.OK).json({ userInfo });
 };
 
 const updateUser = async (req, res) => {
@@ -30,10 +32,11 @@ const updateUser = async (req, res) => {
     throw new BadRequestErr(`there is no user with this id: ${id}`);
   }
 
-  res.status(StatusCodes.CREATED).json(userInfo);
+  res.status(StatusCodes.CREATED).json({ userInfo });
 };
 
 const deleteUser = async (req, res) => {
+  const { id } = req.params;
   const { email, password } = req.body;
   const userInfo = await User.findOne({ email });
   if (!userInfo) {
@@ -45,12 +48,68 @@ const deleteUser = async (req, res) => {
     throw new BadRequestErr("You have passed the wrong password!");
   }
 
-  await User.findByIdAndDelete(userInfo._id);
+  // remove the id from the all friends identifiers list
+  userInfo.friends.forEach(async (friendID) => {
+    let aFriend = await User.findById(friendID);
+    aFriend.friends = aFriend.friends.filter((friendID) => friendID !== id);
+    await aFriend.save();
+  });
+
+  // await userInfo.save();
+
+  await User.findByIdAndDelete(id);
+  // the userInfo constant is keeping the info of that user inside it
   let fuleName = userInfo.firstName + " " + userInfo.lastName;
-  res.status(StatusCodes.CREATED).json(fuleName);
+  res.status(StatusCodes.CREATED).json({ fuleName });
 };
 
-const addFriend = async (req, res) => {};
-const deleteFriend = async (req, res) => {};
+const getAllFriends = async (req, res) => {
+  const { id } = req.params;
 
-module.exports = { getUser, updateUser, deleteUser, addFriend, deleteFriend };
+  const userInfo = await User.findById(id).select("friends");
+  if (!userInfo) {
+    throw new BadRequestErr(`there is no user with this id: ${id}`);
+  }
+
+  // this to return just what we need from the obj
+  const formattedFriends = await formattedFriendsFn(userInfo.friends);
+  res.status(StatusCodes.OK).json({ formattedFriends });
+};
+
+const addRemoveFriend = async (req, res) => {
+  const { id, friendId } = req.params;
+
+  const userInfo = await User.findById(id);
+  if (!userInfo) {
+    throw new BadRequestErr("There is no user with this id!");
+  }
+  const friendInfo = await User.findById(friendId);
+  if (!friendInfo || friendId === id) {
+    throw new BadRequestErr("There is no user with this friend's id!");
+  }
+
+  if (userInfo.friends.includes(friendId)) {
+    userInfo.friends = userInfo.friends.filter(
+      (friendID) => friendID != friendId
+    );
+    friendInfo.friends = friendInfo.friends.filter(
+      (friendID) => friendID != id
+    );
+  } else {
+    userInfo.friends.push(friendId);
+    friendInfo.friends.push(id);
+  }
+  // I think that the password get hashed another time here
+  await userInfo.save();
+  await friendInfo.save();
+  let userFriends = userInfo.friends;
+  res.status(StatusCodes.OK).json({ userFriends });
+};
+
+module.exports = {
+  getUser,
+  updateUser,
+  deleteUser,
+  addRemoveFriend,
+  getAllFriends,
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,12 +2,22 @@ const express = require("express");
 const usersRouter = express.Router();
 
 const authMiddleware = require("../middlewares/auth");
-const { getUser, updateUser, deleteUser } = require("../controllsers/users");
+const {
+  getUser,
+  updateUser,
+  deleteUser,
+  getAllFriends,
+  addRemoveFriend,
+} = require("../controllsers/users");
 
 usersRouter
   .route("/:id")
   .get(authMiddleware, getUser)
   .patch(authMiddleware, updateUser)
   .delete(authMiddleware, deleteUser);
+
+usersRouter.get("/:id/friends", authMiddleware, getAllFriends);
+
+usersRouter.patch("/:id/friends/:friendId", authMiddleware, addRemoveFriend);
 
 module.exports = usersRouter;
