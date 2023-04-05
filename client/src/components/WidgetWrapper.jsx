a22ec26a59d3624ad1af48d68bc5244b52fc660b
sofian-elgharbaoui <sofiangharbaoui54@gmail.com>
2023-04-05 13:34:19 +0000

    correct the comp name

--- client/src/components/WidgetWrapper.jsx
+++ client/src/components/WidgetWrapper.jsx
@@ -1,8 +1,8 @@
 import { Box } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-export const WedgetWrapper = styled(Box)(({ theme }) => ({
+export const WidgetWrapper = styled(Box)(({ theme }) => ({
   padding: 16,
-  borderRadius: 14,
+  borderRadius: 16,
   backgroundColor: theme.palette.background.alt,
 }));
